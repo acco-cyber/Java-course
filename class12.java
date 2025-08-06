1ed3a4f57d677294f0cfd489eb6ef4bf8cdc991a
acco-cyber <koushiktrade750@gmail.com>
2025-08-06 07:17:57 -0600

    Add files via upload

---
+++ class12.java
@@ -0,0 +1,12 @@
+public class class12 {
+    public static void main(String[] args) {
+        // Declare multiple variables in one line
+        int x = 5, y = 6, z = 50;
+        System.out.println(x + y + z);
+
+        // Assign one value to multiple variables
+        int a, b, c;
+        a = b = c = 50;
+        System.out.println(a + b + c);
+    }
+}
