b6720cb18321d6c59e22c32f3dfef8b01ca8b72d
acco-cyber <koushiktrade750@gmail.com>
2025-08-06 07:14:10 -0600

    Add files via upload

---
+++ class11.java
@@ -0,0 +1,18 @@
+public class class11 {
+    public static void main(String[] args) {
+        // Combine text and variable
+        String name = "John";
+        System.out.println("Hello " + name);
+
+        // Combine two variables
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        // Add numeric variables
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y); // Print the value of x + y
+    }
+}
