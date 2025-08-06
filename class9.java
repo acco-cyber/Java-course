7fe1c95b021951e3fa75baffe1a142ce90ab14ce
acco-cyber <koushiktrade750@gmail.com>
2025-08-06 07:08:56 -0600

    Add files via upload

---
+++ class9.java
@@ -0,0 +1,8 @@
+public class class9 {
+      public static void main(String[] args) {
+    System.out.println("Hello World"); // This is a comment
+        /* The code below will print the words Hello World
+    to the screen, and it is amazing */
+    System.out.println("Hello World");
+  }
+}
